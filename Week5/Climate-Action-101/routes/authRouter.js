f5252b6520991d34d6ee0a556b48810e15c37865
shanon-ewingsudds <shannon.ewing-sudds@bryanuniversity.edu>
2022-06-20 16:24:01 -0400

    update

--- Week5/Climate-Action-101/routes/authRouter.js
+++ Week5/Climate-Action-101/routes/authRouter.js
@@ -20,7 +20,7 @@ authRouter.post("/signup", (req, res, next) => {
         res.status(500)
         return next(err)
       }
-      const token = jwt.sign(savedUser.toObject(), process.env.SECRET)
+      const token = jwt.sign(savedUser.withoutPassword(), process.env.SECRET)
       return res.status(201).send({ token, user: savedUser })
     })
   })
@@ -28,6 +28,7 @@ authRouter.post("/signup", (req, res, next) => {
 
 // Login
 authRouter.post("/login", (req, res, next) => {
+  const failedLogin = 'Username or Password is Incorrect'
   User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
     if(err){
       res.status(500)
@@ -35,18 +36,17 @@ authRouter.post("/login", (req, res, next) => {
     }
     if(!user){
       res.status(403)
-      return next(new Error("Username or Password are incorrect"))
+      return next(new Error(failedLogin))
     }
-    
     user.checkPassword(req.body.password, (err, isMatch) => {
       if(err){
         res.status(403)
-        return next(new Error("Username or Password are incorrect"))
-      }
-      if(!isMatch){
-        res.status(403)
-        return next(new Error("Username or Password are incorrect"))
-      }
+        return next(new Error(failedLogin))
+        }
+        if(!isMatch){
+          res.status(403)
+          return next(new Error(failedLogin))
+        }
       const token = jwt.sign(user.withoutPassword(), process.env.SECRET)
       return res.status(200).send({ token, user: user.withoutPassword() })
     })
