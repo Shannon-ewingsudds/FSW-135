b9f5e6d34c57a0f74310ae2a7b53fe6f0e03b4f2
shanon-ewingsudds <shannon.ewing-sudds@bryanuniversity.edu>
2022-06-28 17:56:05 -0400

    Capstone week7

---
+++ Week7/Climate-Action-101/client/src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+export default function PageNotFound() {
+    return (
+        <p style={{backgroundColor: "#c00000", color: "#ffffff", textAlign: "center"}}>Error 404. Page Not Found</p>
+    )
+}
